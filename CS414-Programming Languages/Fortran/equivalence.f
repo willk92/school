f86460fe2363c4d58bbe82c908d1d10ca4460ba7
will <will@sharpauctionengine.com>
2018-10-03 09:20:19 -0500

    adding results to comments section

--- CS414-Programming Languages/Fortran/equivalence.f
+++ CS414-Programming Languages/Fortran/equivalence.f
@@ -19,23 +19,23 @@ program equivalence
    20   CONTINUE
 end program equivalence
 
- Value of A(           1 ) =    1.00000000
- Value of B(           1 ) =    1.00000000
- Value of A(           2 ) =    2.00000000
- Value of B(           2 ) =    2.00000000
- Value of A(           3 ) =    3.00000000
- Value of B(           3 ) =    3.00000000
- Value of A(           4 ) =    4.00000000
- Value of B(           4 ) =    4.00000000
- Value of A(           5 ) =    5.00000000
- Value of B(           5 ) =    5.00000000
- Value of A(           6 ) =    6.00000000
- Value of B(           6 ) =    6.00000000
- Value of A(           7 ) =    7.00000000
- Value of B(           7 ) =    7.00000000
- Value of A(           8 ) =    8.00000000
- Value of B(           8 ) =    8.00000000
- Value of A(           9 ) =    9.00000000
- Value of B(           9 ) =    9.00000000
- Value of A(          10 ) =    10.0000000
- Value of B(          10 ) =    10.0000000
+c  Value of A(           1 ) =    1.00000000
+c  Value of B(           1 ) =    1.00000000
+c  Value of A(           2 ) =    2.00000000
+c  Value of B(           2 ) =    2.00000000
+c  Value of A(           3 ) =    3.00000000
+c  Value of B(           3 ) =    3.00000000
+c  Value of A(           4 ) =    4.00000000
+c  Value of B(           4 ) =    4.00000000
+c  Value of A(           5 ) =    5.00000000
+c  Value of B(           5 ) =    5.00000000
+c  Value of A(           6 ) =    6.00000000
+c  Value of B(           6 ) =    6.00000000
+c  Value of A(           7 ) =    7.00000000
+c  Value of B(           7 ) =    7.00000000
+c  Value of A(           8 ) =    8.00000000
+c  Value of B(           8 ) =    8.00000000
+c  Value of A(           9 ) =    9.00000000
+c  Value of B(           9 ) =    9.00000000
+c  Value of A(          10 ) =    10.0000000
+c  Value of B(          10 ) =    10.0000000
