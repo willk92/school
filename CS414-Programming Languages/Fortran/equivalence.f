c22fa1d7d87c5bc1058fdff23c860fac062a5741
will <will@sharpauctionengine.com>
2018-10-03 09:17:49 -0500

    add equivalence.f completed

---
+++ CS414-Programming Languages/Fortran/equivalence.f
@@ -0,0 +1,41 @@
+c William Kelley
+c Investigation into Equivalence
+c CS414-Programming Languages
+c equivalence.f
+
+program equivalence
+    DIMENSION A(10), B(10)
+    EQUIVALENCE (A, B)
+    INTEGER SIZE
+    SIZE = 10
+
+        DO 10, I = 1, SIZE
+            A(I) = I                    !! Only placing values into Array A
+   10   CONTINUE
+
+        DO 20, I = 1, SIZE
+            PRINT *,"Value of A(", I, ") = ", A(I)
+            PRINT *,"Value of B(", I, ") = ", B(I)
+   20   CONTINUE
+end program equivalence
+
+ Value of A(           1 ) =    1.00000000
+ Value of B(           1 ) =    1.00000000
+ Value of A(           2 ) =    2.00000000
+ Value of B(           2 ) =    2.00000000
+ Value of A(           3 ) =    3.00000000
+ Value of B(           3 ) =    3.00000000
+ Value of A(           4 ) =    4.00000000
+ Value of B(           4 ) =    4.00000000
+ Value of A(           5 ) =    5.00000000
+ Value of B(           5 ) =    5.00000000
+ Value of A(           6 ) =    6.00000000
+ Value of B(           6 ) =    6.00000000
+ Value of A(           7 ) =    7.00000000
+ Value of B(           7 ) =    7.00000000
+ Value of A(           8 ) =    8.00000000
+ Value of B(           8 ) =    8.00000000
+ Value of A(           9 ) =    9.00000000
+ Value of B(           9 ) =    9.00000000
+ Value of A(          10 ) =    10.0000000
+ Value of B(          10 ) =    10.0000000
