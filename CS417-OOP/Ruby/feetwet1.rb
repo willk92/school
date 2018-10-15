0e1354a0aef51bac687598695bd8120d11754f45
will <will@sharpauctionengine.com>
2018-10-15 12:16:02 -0500

    adding requested problem to solve to top of the file

--- CS417-OOP/Ruby/feetwet1.rb
+++ CS417-OOP/Ruby/feetwet1.rb
@@ -1,6 +1,9 @@
 # William Kelley
 # CS417-OOP
-# Getting feet wet with ruby
+# Create a Ruby script that defines a function occurrences(list,key)
+# that takes an array list and object key and returns the number of
+# elements in list that are equal to key. Include sufficient test code
+# to test your program (either by hand or using Ruby's unit test framework).
 # feetwet1.rb
 
 def occurences(list, key)
