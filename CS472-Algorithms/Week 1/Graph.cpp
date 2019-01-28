a05be404d2d9302aed674d2dbbf2b32fe6bbcf81
will <will@sharpauctionengine.com>
2019-01-28 17:14:15 -0600

    finished graph

--- CS472-Algorithms/Week 1/Graph.cpp
+++ CS472-Algorithms/Week 1/Graph.cpp
@@ -4,6 +4,8 @@ OOP ‑ Assignment 2
 Graphs
 Source for Help: A lot of stackoverflow, nothing in particular was used but
 definitely pulled a lot of references and similarities from.
+https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
+https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
 */
 
 #include <iostream>
@@ -37,6 +39,9 @@ public:
 	virtual void deleteNode() { cout << "Parent class 'deleteNode()'" << endl; };
 	virtual void addEdge() { cout << "Parent class 'addEdge()'" << endl; };
 	virtual void deleteEdge() { cout << "Parent class 'deleteEdge()'" << endl; };
+	virtual void BFS() { cout << "Parent class 'BFS()'" << endl; };
+	virtual void DFSUtil() { cout << "Parent class 'DFSUtil()'" << endl; };
+	virtual void DFS() { cout << "Parent class 'DFS()'" << endl; }
 };
 
 template <class T>
@@ -56,6 +61,9 @@ public:
 	void deleteNode(T x);
 	void addEdge(T x, T y);
 	void deleteEdge(T x, T y);
+	void BFS(T v);
+	void DFSUtil(T v, bool visited[]);
+	void DFS(T v);
 };
 
 template <class T>
@@ -75,6 +83,9 @@ public:
 	void deleteNode(T x);
 	void addEdge(T x, T y);
 	void deleteEdge(T x, T y);
+	void BFS(T v);
+	void DFSUtil(T v, bool visited[]);
+	void DFS(T v);
 };
 
 template<class T>
@@ -133,7 +144,17 @@ void AdjacencyMatrix<T>::printGraph(vector<T> x) const
 template<class T>
 bool AdjacencyMatrix<T>::adjacent(T x, T y)
 {
-	return false;
+	T point = (x * 10) + y;
+	std::vector<int>::iterator it;
+	it = find(adjmatrix.begin(), adjmatrix.end(), point);
+	if (it != adjmatrix.end())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 template<class T>
@@ -169,6 +190,7 @@ void AdjacencyMatrix<T>::addNode(T x)
 	T point = x + (x * 10);		// this allows for the user to combine the points into a single value to be stored
 	adjmatrix.erase(std::remove(adjmatrix.begin(), adjmatrix.end(), point), adjmatrix.end());
 	adjmatrix.push_back(point);
+	sort(adjmatrix.begin(), adjmatrix.end());
 }
 
 template<class T>
@@ -178,7 +200,11 @@ void AdjacencyMatrix<T>::deleteNode(T x)
 	for (T i = 0; i <= 9; ++i)
 	{
 		adjmatrix.erase(std::remove(adjmatrix.begin(), adjmatrix.end(), point+i), adjmatrix.end());
+		for (T i = 0; i <= 9; ++i) {
+			adjmatrix.erase(std::remove(adjmatrix.begin(), adjmatrix.end(), ((i * 10) + (point/10))), adjmatrix.end());
+		}
 	}
+	sort(adjmatrix.begin(), adjmatrix.end());
 }
 
 template<class T>
@@ -187,6 +213,7 @@ void AdjacencyMatrix<T>::addEdge(T x, T y)
 	T point = (x * 10) + y;
 	adjmatrix.erase(std::remove(adjmatrix.begin(), adjmatrix.end(), point), adjmatrix.end());	//deleting edge if it exists and re-adding just for ease
 	adjmatrix.push_back(point);
+	sort(adjmatrix.begin(), adjmatrix.end());
 }
 
 template<class T>
@@ -204,6 +231,98 @@ void AdjacencyMatrix<T>::deleteEdge(T x, T y)
 	{
 		cout << "Unable to locate edge." << endl;
 	}
+	sort(adjmatrix.begin(), adjmatrix.end());
+}
+
+template<class T>
+void AdjacencyMatrix<T>::BFS(T v)
+{
+	const int V = 10;
+	T first;
+	T second;
+	vector<int> adj[10];
+	for (auto i = adjmatrix.begin(); i != adjmatrix.end(); ++i)
+	{
+		first = *i / 10;
+		second = *i % 10;
+		adj[first].push_back(second);
+	}
+
+	// Mark all the vertices as not visited
+	bool *visited = new bool[V];
+	for (int i = 0; i < V; i++)
+		visited[i] = false;
+
+	// Create a queue for BFS
+	list<int> queue;
+
+	// Mark the current node as visited and enqueue it
+	visited[v] = true;
+	queue.push_back(v);
+
+	// 'i' will be used to get all adjacent
+	// vertices of a vertex
+	list<int>::iterator i;
+
+	while (!queue.empty())
+	{
+		// Dequeue a vertex from queue and print it
+		v = queue.front();
+		cout << v << " ";
+		queue.pop_front();
+
+		// Get all adjacent vertices of the dequeued
+		// vertex s. If a adjacent has not been visited,
+		// then mark it visited and enqueue it
+		for (auto i = adj[v].begin(); i != adj[v].end(); ++i)
+		{
+			if (!visited[*i])
+			{
+				visited[*i] = true;
+				queue.push_back(*i);
+			}
+		}
+	}
+}
+
+template<class T>
+void AdjacencyMatrix<T>::DFSUtil(T v, bool visited[])
+{
+	const int V = 10;
+	T first;
+	T second;
+	vector<int> adj[10];
+	for (auto i = adjmatrix.begin(); i != adjmatrix.end(); ++i)
+	{
+		first = *i / 10;
+		second = *i % 10;
+		adj[first].push_back(second);
+	}
+
+	// Mark the current node as visited and
+	// print it
+	visited[v] = true;
+	cout << v << " ";
+
+	// Recur for all the vertices adjacent
+	// to this vertex
+	list<int>::iterator i;
+	for (auto i = adj[v].begin(); i != adj[v].end(); ++i)
+		if (!visited[*i])
+			DFSUtil(*i, visited);
+}
+
+template<class T>
+void AdjacencyMatrix<T>::DFS(T v)
+{
+	// Mark all the vertices as not visited
+	bool *visited = new bool[10];
+	for (auto i = 0; i < 10; i++)
+		visited[i] = false;
+
+	// Call the recursive helper function
+	// to print DFS traversal
+	DFSUtil(v, visited);
 }
 
 template<class T>
@@ -263,7 +382,17 @@ void AdjacencyList<T>::printGraph(vector<T> x) const
 template<class T>
 bool AdjacencyList<T>::adjacent(T x, T y)
 {
-	return false;
+	T point = (x * 10) + y;
+	std::list<int>::iterator it;
+	it = find(adjlist.begin(), adjlist.end(), point);
+	if (it != adjlist.end())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 template<class T>
@@ -299,6 +428,7 @@ void AdjacencyList<T>::addNode(T x)
 	T point = x + (x * 10);		// this allows for the user to combine the points into a single value to be stored
 	adjlist.erase(std::remove(adjlist.begin(), adjlist.end(), point), adjlist.end());
 	adjlist.push_back(point);
+	adjlist.sort();
 }
 
 template<class T>
@@ -308,7 +438,12 @@ void AdjacencyList<T>::deleteNode(T x)
 	for (T i = 0; i <= 9; ++i)
 	{
 		adjlist.erase(std::remove(adjlist.begin(), adjlist.end(), point + i), adjlist.end());
+		for (T i = 0; i <= 9; ++i)
+		{
+			adjlist.erase(std::remove(adjlist.begin(), adjlist.end(), ((i*10)+(point/10))), adjlist.end());
+		}
 	}
+	adjlist.sort();
 }
 
 template<class T>
@@ -317,6 +452,7 @@ void AdjacencyList<T>::addEdge(T x, T y)
 	T point = (x * 10) + y;
 	adjlist.erase(std::remove(adjlist.begin(), adjlist.end(), point), adjlist.end());	//deleting edge if it exists and re-adding just for ease
 	adjlist.push_back(point);
+	adjlist.sort();
 }
 
 template<class T>
@@ -334,6 +470,98 @@ void AdjacencyList<T>::deleteEdge(T x, T y)
 	{
 		cout << "Unable to locate edge." << endl;
 	}
+	adjlist.sort();
+}
+
+template<class T>
+void AdjacencyList<T>::BFS(T v)
+{
+	const int V = 10;
+	T first;
+	T second;
+	vector<int> adj[10];
+	for (auto i = adjlist.begin(); i != adjlist.end(); ++i)
+	{
+		first = *i / 10;
+		second = *i % 10;
+		adj[first].push_back(second);
+	}
+
+	// Mark all the vertices as not visited
+	bool *visited = new bool[V];
+	for (int i = 0; i < V; i++)
+		visited[i] = false;
+
+	// Create a queue for BFS
+	list<int> queue;
+
+	// Mark the current node as visited and enqueue it
+	visited[v] = true;
+	queue.push_back(v);
+
+	// 'i' will be used to get all adjacent
+	// vertices of a vertex
+	list<int>::iterator i;
+
+	while (!queue.empty())
+	{
+		// Dequeue a vertex from queue and print it
+		v = queue.front();
+		cout << v << " ";
+		queue.pop_front();
+
+		// Get all adjacent vertices of the dequeued
+		// vertex s. If a adjacent has not been visited,
+		// then mark it visited and enqueue it
+		for (auto i = adj[v].begin(); i != adj[v].end(); ++i)
+		{
+			if (!visited[*i])
+			{
+				visited[*i] = true;
+				queue.push_back(*i);
+			}
+		}
+	}
+}
+
+template<class T>
+void AdjacencyList<T>::DFSUtil(T v, bool visited[])
+{
+	const int V = 10;
+	T first;
+	T second;
+	vector<int> adj[10];
+	for (auto i = adjlist.begin(); i != adjlist.end(); ++i)
+	{
+		first = *i / 10;
+		second = *i % 10;
+		adj[first].push_back(second);
+	}
+
+	// Mark the current node as visited and
+	// print it
+	visited[v] = true;
+	cout << v << " ";
+
+	// Recur for all the vertices adjacent
+	// to this vertex
+	list<int>::iterator i;
+	for (auto i = adj[v].begin(); i != adj[v].end(); ++i)
+		if (!visited[*i])
+			DFSUtil(*i, visited);
+}
+
+template<class T>
+void AdjacencyList<T>::DFS(T v)
+{
+	// Mark all the vertices as not visited
+	bool *visited = new bool[10];
+	for (auto i = 0; i < 10; i++)
+		visited[i] = false;
+
+	// Call the recursive helper function
+	// to print DFS traversal
+	DFSUtil(v, visited);
 }
 
 
@@ -405,6 +633,30 @@ int main()
 	while (inputX != 0) {
 		matrix.printGraph(matrix.neighbors(inputX));
 		cout << "\nEnter another node to find out neighbors or enter 0 to exit\n";
+		cin >> inputX;
+	}
+
+	cout << "Enter nodes to determine whether or not they are adjacent (enter x,y values seperated by a space or 0 0 to exit)\n";
+	cin >> inputX >> inputY;
+	while (inputX != 0) {
+		matrix.adjacent(inputX, inputY);
+		cout << "\nEnter another edge to determine whether it has adjacency (enter x,y values separated by a space or 0 0 to exit)\n";
+		cin >> inputX >> inputY;
+	}
+
+	cout << "What BFS vertex would you like to know the nodes of? (enter node or 0 to exit)\n";
+	cin >> inputX;
+	while (inputX != 0) {
+		matrix.BFS(inputX);
+		cout << "\nEnter another node to find BFS of or enter 0 to exit\n";
+		cin >> inputX;
+	}
+
+	cout << "What DFS vertex would you like to know the nodes of? (enter node or 0 to exit)\n";
+	cin >> inputX;
+	while (inputX != 0) {
+		matrix.DFS(inputX);
+		cout << "\nEnter another node to find BFS of or enter 0 to exit\n";
 		cin >> inputX;
 	}
 
