e65d186a2291be7fed7313d366ba5546cb05908e
willk92 <wkelley1992@gmail.com>
2019-06-08 16:47:36 -0500

    stuff

--- ITE315-Scripting/Assn04/assn04.part01.ps1
+++ ITE315-Scripting/Assn04/assn04.part01.ps1
@@ -1,3 +1,6 @@
-﻿Write-Host "$args"
+﻿# Convert Strihttps://social.technet.microsoft.com/wiki/contents/articles/25071.powershell-tip-converting-string-to-character-array.aspxng into ASCII
+# Source: 
+
+Write-Host "$args"
 
 $args.ToCharArray() | %{[int][char]$_}
---
+++ ITE315-Scripting/Assn04/assn04.part03.ps1
@@ -0,0 +1,11 @@
+﻿#Display  week number of specific date or if none entered display current week
+
+if ( $args -ne "" ) {
+    $day = [DateTime]"$args"
+    [decimal]$day_number = $day.DayOfYear
+    $week_number = [Math]::Round([Math]::Ceiling($day_number/7))
+    Write-Host $week_number
+}
+else {
+    Get-Date -UFormat %V
+}
