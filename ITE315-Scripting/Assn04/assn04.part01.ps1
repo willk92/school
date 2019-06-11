048b9994370afbdd053786012f74b930b1562785
willk92 <wkelley1992@gmail.com>
2019-06-10 21:46:43 -0500

    lab11

--- ITE315-Scripting/Assn04/assn04.part01.ps1
+++ ITE315-Scripting/Assn04/assn04.part01.ps1
@@ -1,5 +1,5 @@
-﻿# Convert Strihttps://social.technet.microsoft.com/wiki/contents/articles/25071.powershell-tip-converting-string-to-character-array.aspxng into ASCII
-# Source: 
+﻿# Convert String to ASCII
+# Source: https://social.technet.microsoft.com/wiki/contents/articles/25071.powershell-tip-converting-string-to-character-array.aspxng into ASCII
 
 Write-Host "$args"
 
